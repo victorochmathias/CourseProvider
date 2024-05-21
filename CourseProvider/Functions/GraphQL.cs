8908af497a369301e366543aa2d5f02fbec0279e
Mathias Rönnmark <mathiasronnmark@hotmail.com>
2024-05-21 13:09:09 +0200

    ...

--- CourseProvider/Functions/GraphQL.cs
+++ CourseProvider/Functions/GraphQL.cs
@@ -11,7 +11,7 @@ public class GraphQL(ILogger<GraphQL> logger, IGraphQLRequestExecutor executor)
     private readonly IGraphQLRequestExecutor _executor = executor;
 
     [Function("GraphQL")]
-    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "graphql")] HttpRequest req)
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "graphql")] HttpRequest req)
     {
         return await _executor.ExecuteAsync(req);
     }
